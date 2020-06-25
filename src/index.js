2bd2936b3142601081e6c34c33aab8b1739052eb
TomSssM <ilyashome3@gmail.com>
2020-06-25 19:50:33 +0300

    feat: solve kata

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+function longestConsecutiveLength(array) {
+  const step = 1;
+  const set = new Set();
+  let longestSequence = 0;
+
+  array.forEach((val) => {
+    let sequence = 1;
+    let current;
+
+    set.add(val);
+
+    current = val;
+
+    while (set.has(current + step)) {
+      current += step;
+      sequence += 1;
+    }
+
+    current = val;
+
+    while (set.has(current - step)) {
+      sequence += 1;
+      current -= step;
+    }
+
+    if (sequence > longestSequence) {
+      longestSequence = sequence;
+    }
+  });
+
+  return longestSequence;
 }
+
+module.exports = longestConsecutiveLength;
